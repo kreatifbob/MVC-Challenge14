c4312b35055ae9d68f4e1449423a6a5a0954164d
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-08-31 15:09:18 +1000

    Update project.js

---
+++ models/project.js
@@ -0,0 +1,42 @@
+const Sequelize = require("sequelize");
+const sequelizeConnection = require("../config/sequelizeConnection");
+
+const Post = sequelizeConnection.define(
+  "post",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      reference: {
+        model: "User",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize: sequelizeConnection,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: "posts",
+    underscored: true,
+  }
+);
+
+module.exports = Post;
